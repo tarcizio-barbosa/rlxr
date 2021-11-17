ced52aafc8d5c5082c3b5f5b387aeb729aae3ba3
Tarcizio Barbosa - Dislog <tarcizio.barbosa@outlook.com>
2021-11-17 17:33:13 -0300

    fix the navs alignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import styles from './App.module.scss';
+import { IoChevronDown } from 'react-icons/io5';
+
 import { NavLink } from './components/NavLink';
+
+import styles from './App.module.scss';
 
 export function App() {
   return (
     <div className={styles.appWrapper}>
-      <NavLink linkName="Home" />
+      <div className={styles.navOneContainer}>
+        <NavLink linkName="Home" />
+        <NavLink linkName="Contact Us" />
+        <NavLink linkName="Help" />
+      </div>
+      <div className={styles.navTwoContainer}>
+        <div className={styles.iconWapper}>
+          <NavLink linkName="English" />
+          <IoChevronDown size={18} color="#D95A5F" />
+        </div>
+        <NavLink linkName="Login" />
+        <NavLink linkName="Register" />
+      </div>
     </div>
   );
 }
